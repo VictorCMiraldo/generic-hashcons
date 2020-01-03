acaf0cb36a007707ffb5d990c23c6e6b15deb200
Victor Miraldo <v.cacciarimiraldo@gmail.com>
2020-01-03 17:05:33 +0100

    Holy moly, it works! Next step, define diff?

--- src/GHC/Generic/Merkle.hs
+++ src/GHC/Generic/Merkle.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE QuantifiedConstraints #-}
 {-# LANGUAGE DeriveGeneric #-}
@@ -18,6 +21,8 @@ import Data.Proxy
 import GHC.Generics
 import GHC.TypeLits
 
+import Control.Arrow (first , (&&&), (***))
+
 import Data.Word (Word8,Word64)
 import Data.Bits
 import Data.List (splitAt,foldl')
@@ -30,6 +35,8 @@ import qualified Crypto.Hash            as Hash
 import qualified Crypto.Hash.Algorithms as Hash (Blake2s_256)
 
 import qualified Data.Map as M
+
+import Unsafe.Coerce
 
 -- |Our digests come from Blake2s_256 
 newtype Digest
@@ -69,60 +76,91 @@ digestSalt str d = digestConcat [hashStr str , d]
 ----------------------------
 -- The Monster
 
-data Dig1 x = Dig1 Digest
+type family IsElem (a :: *) (as :: [ * ]) :: Bool where
+  IsElem a (a ': as) = 'True
+  IsElem a (b ': as) = IsElem a as
+  IsElem a '[]       = 'False
 
-data Dig rep :: * -> * where
-  DigD1 :: Digest -> Dig (K1 R a) x
-  DigK1 :: (Show a) => a -> Dig (K1 R a) x
-  DigM1 :: Dig f x  -> Dig (M1 i c f) x
-  DigU1 :: Dig U1 x
-  DigPair :: Dig f x -> Dig g x -> Dig (f :*: g) x
-  DigL1 :: Dig f x ->  Dig (f :+: g) x
-  DigR1 :: Dig g x ->  Dig (f :+: g) x 
+type Elem    a as = IsElem a as ~ 'True
+type NotElem a as = IsElem a as ~ 'False
 
-deriving instance Show (Dig rep x)
+-- A Value of type @Dig prim rep@ represents one layer of
+-- rep and, for the atoms of rep that are not elems of
+-- the primitive types, a hash pointer.
+data Dig (prim :: [ * ]) rep :: * -> * where
+  DigD1   :: (NotElem a prim)       => Digest -> Dig prim (K1 R a) x
+  DigK1   :: (Elem a prim , Show a) => a      -> Dig prim (K1 R a) x
+  DigU1   :: Dig prim U1 x
+  DigM1   :: Dig prim f x -> Dig prim (M1 i c f) x
+  DigL1   :: Dig prim f x -> Dig prim (f :+: g) x
+  DigR1   :: Dig prim g x -> Dig prim (f :+: g) x 
+  DigPair :: Dig prim f x -> Dig prim g x -> Dig prim (f :*: g) x
+deriving instance Show (Dig prim rep x)
 
-
-data ExRep where
+-- Wrap it in an existential
+data ExRep prim where
   ExRep :: (Generic a)
-        => Proxy a -> Dig (Rep a) x -> ExRep
+        => Proxy a -> Dig prim (Rep a) x -> ExRep prim
 
-instance Show ExRep where
+instance Show (ExRep prim) where
   show (ExRep _ x) = show x
 
-type HashConsed
-  = M.Map Digest ExRep
+-- And define a hash-consed value to be a map from
+-- digest into existential rep.
+type HashConsed prim
+  = M.Map Digest (ExRep prim)
 
-class HashCons a where
-  hashCons :: a -> (Digest , HashConsed)
-  default hashCons :: (Generic a , GHashCons (Rep a)
+-- HashConsing then consists in traversing the
+-- value; returning the digest of the root and
+-- a database of hashconsed subtrees!
+--
+-- TODO: This will be better in a state monad; where
+-- we dont need to do M.unions on products
+class HashCons prim a where
+  hashCons :: a -> (Digest , HashConsed prim)
+  default hashCons :: (Generic a , GHashCons prim (Rep a)
                       )
-                   => a -> (Digest , HashConsed)
+                   => a -> (Digest , HashConsed prim)
   hashCons a = let (digA , m) = ghashCons (from a)
                    root       = ghashDig (Proxy :: Proxy (Rep a)) digA
                 in (root , M.insert root (ExRep (Proxy :: Proxy a) digA) m)
 
-class GHashCons rep where
-  ghashDig  :: Proxy rep -> Dig rep x -> Digest
-  ghashCons :: rep x -> (Dig rep x , HashConsed)
+-- The usual /default signature' trick for GHC generics with a twist.
+-- While we traverse down the structure of the generic type,
+-- we construct a @Dig prim rep x@.
+class GHashCons prim rep where
+  ghashDig  :: Proxy rep -> Dig prim rep x -> Digest
+  ghashCons :: rep x -> (Dig prim rep x , HashConsed prim)
 
-instance {-# OVERLAPPING #-} GHashCons (K1 R Int) where
-  ghashDig _ (DigK1 i) = hashStr (show i)
-  ghashCons (K1 i) = (DigK1 i , M.empty)
-  
-instance {-# OVERLAPPABLE #-} (HashCons a)
-    => GHashCons (K1 R a) where
-  ghashDig _ (DigD1 d) = d
-  ghashCons (K1 a) =
-    let (root , m) = hashCons a
-     in (DigD1 root , m)
+-- When we reach a @K1 R a@, we must know whether this
+-- type @a@ is a primitive type or not. In case it is, we
+-- compute its digest and keep its value there.
+-- In case its not, we use the 'HashCons' instance; which
+-- inserts a value in the database.
+class GHashConsK1 prim (isPrim :: Bool) a where
+  ghashDigK1  :: Proxy isPrim -> Dig prim (K1 R a) x -> Digest
+  ghashConsK1 :: Proxy isPrim -> (K1 R a) x -> (Dig prim (K1 R a) x , HashConsed prim)
 
-instance GHashCons U1 where
+instance (Show a , Elem a prim) => GHashConsK1 prim 'True a where
+  ghashDigK1 _  (DigK1 i) = hashStr (show i)
+  ghashConsK1 _ (K1 i)    = (DigK1 i , M.empty)
+
+instance (NotElem a prim , HashCons prim a) => GHashConsK1 prim 'False a where
+  ghashDigK1 _ (DigD1 d) = d
+  ghashConsK1 _ (K1 a)   = first DigD1 $ hashCons a
+
+-- And this is where we decide whether a is primitive or not.
+-- Old trick, but fancy as hell! :D
+instance (GHashConsK1 prim (IsElem a prim) a) => GHashCons prim (K1 R a) where
+  ghashDig _ = ghashDigK1  (Proxy :: Proxy (IsElem a prim))
+  ghashCons  = ghashConsK1 (Proxy :: Proxy (IsElem a prim))
+
+instance GHashCons prim U1 where
   ghashDig  _ DigU1 = hashStr "U1"
-  ghashCons   U1 = (DigU1 , M.empty)
+  ghashCons   U1    = (DigU1 , M.empty)
 
-instance (GHashCons f , GHashCons g)
-    => GHashCons (f :*: g) where
+instance (GHashCons prim f , GHashCons prim g)
+    => GHashCons prim (f :*: g) where
   ghashDig _ (DigPair pf pg) = digestConcat
     [ ghashDig (Proxy :: Proxy f) pf
     , ghashDig (Proxy :: Proxy g) pg
@@ -133,52 +171,41 @@ instance (GHashCons f , GHashCons g)
           (pg' , mg) = ghashCons pg
        in (DigPair pf' pg' , M.union mf mg)
 
-instance (GHashCons f , GHashCons g)
-    => GHashCons (f :+: g) where
+instance (GHashCons prim f , GHashCons prim g)
+    => GHashCons prim (f :+: g) where
   ghashDig _ (DigL1 f) = ghashDig (Proxy :: Proxy f) f
   ghashDig _ (DigR1 g) = ghashDig (Proxy :: Proxy g) g
 
-  ghashCons (L1 f) =
-    let (dp , m) = ghashCons f
-     in (DigL1 dp , m)
-  ghashCons (R1 f) =
-    let (dp , m) = ghashCons f
-     in (DigR1 dp , m)
-  
+  ghashCons (L1 f) = first DigL1 $ ghashCons f
+  ghashCons (R1 g) = first DigR1 $ ghashCons g
 
-instance (KnownSymbol dnn , GHashCons p)
-    => GHashCons (D1 ('MetaData dn dnn m f) p) where
+instance (KnownSymbol dnn , GHashCons prim p)
+    => GHashCons prim (D1 ('MetaData dn dnn m f) p) where
   ghashDig _ (DigM1 p) =
     let hp  = ghashDig (Proxy :: Proxy p) p
      in digestSalt (symbolVal (Proxy :: Proxy dnn)) hp
 
-  ghashCons (M1 p) =
-    let (dp , m) = ghashCons p
-     in (DigM1 dp , m)
+  ghashCons (M1 p) = first DigM1 $ ghashCons p
 
-instance (KnownSymbol cn , GHashCons p)
-    => GHashCons (C1 ('MetaCons cn m f) p) where
+instance (KnownSymbol cn , GHashCons prim p)
+    => GHashCons prim (C1 ('MetaCons cn m f) p) where
   ghashDig _ (DigM1 p) =
     let hp  = ghashDig (Proxy :: Proxy p) p
      in digestSalt (symbolVal (Proxy :: Proxy cn)) hp
 
-  ghashCons (M1 p) =
-    let (dp , m) = ghashCons p
-     in (DigM1 dp , m)
+  ghashCons (M1 p) = first DigM1 $ ghashCons p
 
-instance (GHashCons p)
-    => GHashCons (S1 s p) where
+instance (GHashCons prim p)
+    => GHashCons prim (S1 s p) where
   ghashDig _ (DigM1 p) = ghashDig (Proxy :: Proxy p) p
-  ghashCons (M1 p) =
-    let (dp , m) = ghashCons p
-     in (DigM1 dp , m)
-
+  ghashCons (M1 p) = first DigM1 $ ghashCons p
 
 ----------------------------
+-- Simple test:
 
 data BinT
   = Bin BinT BinT
-  | Leaf Int
+  | Leaf Bool
   | Other UnT
   deriving (Eq , Show , Generic)
 
@@ -187,10 +214,40 @@ data UnT
   | ST BinT
   deriving (Eq , Show , Generic)
 
-instance HashCons BinT
-instance HashCons UnT
+type Prims = '[ Int , Bool , Float ]
 
-v = Bin (Leaf 10) (Other (ZT 10))
-                      
+instance HashCons Prims BinT
+instance HashCons Prims UnT
 
+v :: BinT
+v = Bin (Leaf False) (Other (ZT 10))
 
+hcp :: HashCons Prims a => a -> (Digest , HashConsed Prims)
+hcp = hashCons
+
+v' = let (root , m) = hcp v
+      in hashUncons (Proxy :: Proxy BinT) root m
+
+----------------------------------
+-- Reconstructing a value?
+--
+-- Possible with the help of one unsafeCoerce
+-- to let GHC rest that we know what we are doing
+
+dig2rep :: Dig prim f x -> HashConsed prim -> Maybe (f x)
+dig2rep (DigK1 x) _  = return (K1 x)
+dig2rep DigU1     _  = return U1
+dig2rep (DigD1 d) db = K1 <$> hashUncons Proxy d db
+dig2rep (DigM1 m) db = M1 <$> dig2rep m db
+dig2rep (DigL1 x) db = L1 <$> dig2rep x db
+dig2rep (DigR1 y) db = R1 <$> dig2rep y db
+dig2rep (DigPair x y) db = (:*:) <$> dig2rep x db <*> dig2rep y db
+
+proxyTo :: (Generic a) => Proxy a -> Rep a x -> a
+proxyTo _ = to
+
+hashUncons :: Proxy a -> Digest -> HashConsed prim -> Maybe a
+hashUncons _ root db = do
+  (ExRep ty' r) <- M.lookup root db
+  aux <- proxyTo ty' <$> dig2rep r db
+  return $ unsafeCoerce aux -- how do we like this unsafeCoerce here? haha
