33dbef37fdfda9b2ed87e9369b0133b762f5c9cf
Victor Miraldo <v.cacciarimiraldo@gmail.com>
2020-01-07 17:27:05 +0100

    Rename to match alejandros

--- src/GHC/Generic/Deep.hs
+++ src/GHC/Generic/Deep.hs
@@ -34,25 +34,25 @@ type family IsElem (a :: *) (as :: [ * ]) :: Bool where
 type Elem    a as = IsElem a as ~ 'True
 type NotElem a as = IsElem a as ~ 'False
 
--- A Value of type @REP prim f rep@ represents one layer of
+-- A Value of type @SRep prim f rep@ represents one layer of
 -- rep and, for the atoms of rep that are not elems of
 -- the primitive types, some custom data dictated by a functor f.
 -- You know where this is going.
-data REP (prim :: [ * ]) f (rep :: * -> *) :: * where
-  REPD1   :: (NotElem a prim)       => f a -> REP prim f (K1 R a) 
-  REPK1   :: (Elem a prim , Show a) => a   -> REP prim f (K1 R a)
-  REPU1   :: REP prim f U1
-  REPM1   :: REP prim f a -> REP prim f (M1 i c a)
-  REPL1   :: REP prim f a -> REP prim f (a :+: b) 
-  REPR1   :: REP prim f b -> REP prim f (a :+: b)  
-  REPPair :: REP prim f a -> REP prim f b -> REP prim f (a :*: b) 
-deriving instance (forall a . Show (f a)) => Show (REP prim f rep)
+data SRep (prim :: [ * ]) f (rep :: * -> *) :: * where
+  S_D1   :: (NotElem a prim)       => f a -> SRep prim f (K1 R a) 
+  S_K1   :: (Elem a prim , Show a) => a   -> SRep prim f (K1 R a)
+  S_U1   :: SRep prim f U1
+  S_M1   :: SRep prim f a -> SRep prim f (M1 i c a)
+  S_L1   :: SRep prim f a -> SRep prim f (a :+: b) 
+  S_R1   :: SRep prim f b -> SRep prim f (a :+: b)  
+  (:**:) :: SRep prim f a -> SRep prim f b -> SRep prim f (a :*: b) 
+deriving instance (forall a . Show (f a)) => Show (SRep prim f rep)
 
 -- This is where this is going! :D
 data FixAnn prim ann a :: * where
   FixAnn :: (Generic a)
          => { getAnn :: ann a 
-            , unFix  :: REP prim (FixAnn prim ann) (Rep a)
+            , unFix  :: SRep prim (FixAnn prim ann) (Rep a)
             }
          -> FixAnn prim ann a
 deriving instance (forall a . Show (ann a)) => Show (FixAnn prim ann x)
@@ -63,30 +63,30 @@ type Fix prim = FixAnn prim (Const ())
 
 mapRepM :: (Monad m)
         => (forall y . f y -> m (g y))
-        -> REP prim f rep -> m (REP prim g rep)
-mapRepM _f (REPK1 x) = return (REPK1 x)
-mapRepM _f (REPU1)   = return REPU1
-mapRepM f (REPD1 x)  = REPD1 <$> f x
-mapRepM f (REPM1 x)  = REPM1 <$> mapRepM f x
-mapRepM f (REPL1 x)  = REPL1 <$> mapRepM f x
-mapRepM f (REPR1 x)  = REPR1 <$> mapRepM f x
-mapRepM f (REPPair x y)
-  = REPPair <$> mapRepM f x <*> mapRepM f y
+        -> SRep prim f rep -> m (SRep prim g rep)
+mapRepM _f (S_K1 x) = return (S_K1 x)
+mapRepM _f (S_U1)   = return S_U1
+mapRepM f (S_D1 x)  = S_D1 <$> f x
+mapRepM f (S_M1 x)  = S_M1 <$> mapRepM f x
+mapRepM f (S_L1 x)  = S_L1 <$> mapRepM f x
+mapRepM f (S_R1 x)  = S_R1 <$> mapRepM f x
+mapRepM f (x :**: y)
+  = (:**:) <$> mapRepM f x <*> mapRepM f y
 
 mapRep :: (forall y . f y -> g y)
-       -> REP prim f rep -> REP prim g rep
+       -> SRep prim f rep -> SRep prim g rep
 mapRep f = runIdentity . mapRepM (return . f)
         
 cataM :: (Monad m)
       => (forall b x . Generic b
-            => ann b -> REP prim phi (Rep b) -> m (phi b))
+            => ann b -> SRep prim phi (Rep b) -> m (phi b))
       -> FixAnn prim ann a
       -> m (phi a)
 cataM f (FixAnn ann x) = mapRepM (cataM f) x >>= f ann
 
 synthesizeM :: (Monad m)
             => (forall b x . Generic b
-                => ann b -> REP prim phi (Rep b) -> m (phi b))
+                => ann b -> SRep prim phi (Rep b) -> m (phi b))
             -> FixAnn prim ann a
             -> m (FixAnn prim phi a)
 synthesizeM f = cataM (\ann r -> flip FixAnn r <$> f ann (mapRep getAnn r))
@@ -108,7 +108,7 @@ class (Generic a) => Deep prim a where
   dto (FixAnn _ x) = to . gdto $ x
 
 -- Typesyn to make life easier
-type FIX prim = REP prim (Fix prim)
+type FIX prim = SRep prim (Fix prim)
 
 -- Your usual suspect; the GDeep typeclass
 class GDeep prim f where
@@ -123,12 +123,12 @@ class GDeepAtom prim (isPrim :: Bool) a where
   gdtoAtom    :: Proxy isPrim -> FIX prim (K1 R a) -> a
 
 instance (NotElem a prim , Deep prim a) => GDeepAtom prim 'False a where
-  gdfromAtom _ a = REPD1 (dfrom a)
-  gdtoAtom _ (REPD1 x) = dto x
+  gdfromAtom _ a = S_D1 (dfrom a)
+  gdtoAtom _ (S_D1 x) = dto x
 
 instance (Elem a prim , Show a) => GDeepAtom prim 'True a where
-  gdfromAtom _ a = (REPK1 a)
-  gdtoAtom   _ (REPK1 a) = a
+  gdfromAtom _ a = (S_K1 a)
+  gdtoAtom   _ (S_K1 a) = a
 
 -- This ties the recursive knot
 instance (GDeepAtom prim (IsElem a prim) a) => GDeep prim (K1 R a) where
@@ -137,23 +137,23 @@ instance (GDeepAtom prim (IsElem a prim) a) => GDeep prim (K1 R a) where
 
 -- The rest of the instances are trivial
 instance GDeep prim U1 where
-  gdfrom U1  = REPU1
-  gdto REPU1 = U1
+  gdfrom U1  = S_U1
+  gdto S_U1 = U1
 
 instance (GDeep prim f) => GDeep prim (M1 i c f) where
-  gdfrom (M1 x) = REPM1 (gdfrom x)
-  gdto (REPM1 x) = M1 (gdto x)
+  gdfrom (M1 x) = S_M1 (gdfrom x)
+  gdto (S_M1 x) = M1 (gdto x)
 
 instance (GDeep prim f , GDeep prim g) => GDeep prim (f :*: g) where
-  gdfrom (x :*: y) = REPPair (gdfrom x) (gdfrom y)
-  gdto (REPPair x y) = (gdto x) :*: (gdto y)
+  gdfrom (x :*: y) = gdfrom x :**: gdfrom y
+  gdto (x :**: y) = (gdto x) :*: (gdto y)
 
 instance (GDeep prim f , GDeep prim g) => GDeep prim (f :+: g) where
-  gdfrom (L1 x) = REPL1 (gdfrom x)
-  gdfrom (R1 x) = REPR1 (gdfrom x)
+  gdfrom (L1 x) = S_L1 (gdfrom x)
+  gdfrom (R1 x) = S_R1 (gdfrom x)
 
-  gdto (REPL1 x) = L1 (gdto x)
-  gdto (REPR1 x) = R1 (gdto x)
+  gdto (S_L1 x) = L1 (gdto x)
+  gdto (S_R1 x) = R1 (gdto x)
 
 
 ---------------------------
@@ -203,6 +203,7 @@ dfromPrim = dfrom
 
 -- * Unification and Anti Unification
 
+{-
 
 data ((f :: (* -> *) -> *) :**: (g :: (* -> *) -> *)) rep
   = f rep :**: g rep
@@ -210,6 +211,8 @@ data ((f :: (* -> *) -> *) :**: (g :: (* -> *) -> *)) rep
 data Down (f :: (* -> *) -> *) x = Down (f (K1 R x))
 
 antiunif :: (NotElem x prim)
-         => REP prim f (K1 R x) -> REP prim g (K1 R x)
-         -> REP prim (Down (REP prim f :**: REP prim g)) (K1 R x)
-antiunif r s = REPD1 (Down (r :**: s))
+         => SRep prim f (K1 R x) -> SRep prim g (K1 R x)
+         -> SRep prim (Down (SRep prim f :**: SRep prim g)) (K1 R x)
+antiunif r s = S_D1 (Down (r :**: s))
+
+-}
