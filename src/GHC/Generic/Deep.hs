82ef8a308f05a9cdaea8423af60f9bcc01ba2402
Victor Miraldo <v.cacciarimiraldo@gmail.com>
2020-01-07 09:57:43 +0100

    Renamed to match Aljenadro's simplistic-generic; added decidability of IsElem

--- src/GHC/Generic/Deep.hs
+++ src/GHC/Generic/Deep.hs
@@ -35,81 +35,104 @@ type NotElem a as = IsElem a as ~ 'False
 -- rep and, for the atoms of rep that are not elems of
 -- the primitive types, some custom data dictated by a functor f.
 -- You know where this is going.
-data REP (prim :: [ * ]) f rep :: * -> * where
-  REPD1   :: (NotElem a prim)       => f a -> REP prim f (K1 R a) x
-  REPK1   :: (Elem a prim , Show a) => a   -> REP prim f (K1 R a) x
-  REPU1   :: REP prim f U1 x
-  REPM1   :: REP prim f a x -> REP prim f (M1 i c a) x
-  REPL1   :: REP prim f a x -> REP prim f (a :+: b) x
-  REPR1   :: REP prim f b x -> REP prim f (a :+: b) x 
-  REPPair :: REP prim f a x -> REP prim f b x -> REP prim f (a :*: b) x
-deriving instance (forall a . Show (f a)) => Show (REP prim f rep x)
+infixr 5 :**:
+data SRep w f where
+  S_U1   ::                          SRep w U1
+  S_L1   ::              SRep w f -> SRep w (f :+: g)
+  S_R1   ::              SRep w g -> SRep w (f :+: g)
+  (:**:) ::  SRep w f -> SRep w g -> SRep w (f :*: g)
+  S_K1   ::              w a      -> SRep w (K1 i a)
+  S_M1   ::              SRep w f -> SRep w (M1 i t f)
+--  S_ST   ::         c => SRep w f -> SRep w (c :=>: f)
+deriving instance (forall a. Show (w a)) => Show (SRep w f)
 
--- This is where this is going! :D
-data Fix prim a :: * where
-  Fix :: (Generic a) => { unFix :: REP prim (Fix prim) (Rep a) x } -> Fix prim a
-deriving instance Show (Fix prim a)
+-- TODO: spli this type in two; Fix (WithPrim ...)
+data SFix prim a where
+  Prim :: (Elem a prim , Show a)
+       => a -> SFix prim a
+  SFix :: (NotElem a prim , Generic a)
+       => SRep (SFix prim) (Rep a)
+       -> SFix prim a
+deriving instance Show (SFix prim a)
+
+data ElemPrf a as where
+  EP_Yes :: Elem a as    => ElemPrf a as
+  EP_No  :: NotElem a as => ElemPrf a as
+
+class DecideElem a as where
+  isElem :: ElemPrf a as
+
+instance DecideElem a '[] where
+  isElem = EP_No
+
+instance {-# OVERLAPPING #-} DecideElem a (a ': as) where
+  isElem = EP_Yes
+
+instance {-# OVERLAPPABLE #-}
+    (DecideElem a as) => DecideElem a (b ': as) where
+  isElem = isElem
 
 -- Converting values to deep representations is easy and follows
 -- almost the usual convention; one top level class
 -- and one generic version. This time though, we need
 -- special treatment on atoms.
 class (Generic a) => Deep prim a where
-  dfrom :: a -> Fix prim a
-  default dfrom :: (GDeep prim (Rep a)) => a -> Fix prim a
-  dfrom = Fix . gdfrom . from
+  dfrom :: a -> SFix prim a
+  default dfrom :: (Show a , DecideElem a prim , GDeep prim (Rep a))
+                => a -> SFix prim a
+  dfrom = case isElem :: ElemPrf a prim of
+            EP_No  -> SFix . gdfrom . from
+            EP_Yes -> Prim
   
-  dto :: Fix prim a -> a
-  default dto :: (GDeep prim (Rep a)) => Fix prim a -> a
-  dto (Fix x) = to . gdto $ x
-
--- Typesyn to make life easier
-type FIX prim = REP prim (Fix prim)
+  dto :: SFix prim a -> a
+  default dto :: (GDeep prim (Rep a)) => SFix prim a -> a
+  dto (SFix x) = to . gdto $ x
+  dto (Prim a) = a
 
 -- Your usual suspect; the GDeep typeclass
 class GDeep prim f where
-  gdfrom :: f x -> FIX prim f x
-  gdto   :: FIX prim f x -> f x
+  gdfrom :: f x -> SRep (SFix prim) f 
+  gdto   :: SRep (SFix prim) f -> f x 
 
 -- And the class that disambiguates primitive types
 -- from types in the family. This is completely hidden from
 -- the user though
 class GDeepAtom prim (isPrim :: Bool) a where
-  gdfromAtom  :: Proxy isPrim -> a -> FIX prim (K1 R a) x
-  gdtoAtom    :: Proxy isPrim -> FIX prim (K1 R a) x -> a
+  gdfromAtom  :: Proxy isPrim -> a -> SFix prim a
+  gdtoAtom    :: Proxy isPrim -> SFix prim a -> a
 
 instance (NotElem a prim , Deep prim a) => GDeepAtom prim 'False a where
-  gdfromAtom _ a = REPD1 (dfrom a)
-  gdtoAtom _ (REPD1 x) = dto x
+  gdfromAtom _ a = dfrom a
+  gdtoAtom _   x = dto x
 
 instance (Elem a prim , Show a) => GDeepAtom prim 'True a where
-  gdfromAtom _ a = (REPK1 a)
-  gdtoAtom   _ (REPK1 a) = a
+  gdfromAtom _ a = (Prim a)
+  gdtoAtom   _ (Prim a) = a
 
 -- This ties the recursive knot
 instance (GDeepAtom prim (IsElem a prim) a) => GDeep prim (K1 R a) where
-  gdfrom (K1 a)  = gdfromAtom (Proxy :: Proxy (IsElem a prim)) a
-  gdto   a       = K1 (gdtoAtom (Proxy :: Proxy (IsElem a prim)) a)
+  gdfrom (K1 a)   = S_K1 (gdfromAtom (Proxy :: Proxy (IsElem a prim)) a)
+  gdto   (S_K1 a) = K1 (gdtoAtom (Proxy :: Proxy (IsElem a prim)) a)
 
 -- The rest of the instances are trivial
 instance GDeep prim U1 where
-  gdfrom U1  = REPU1
-  gdto REPU1 = U1
+  gdfrom U1  = S_U1
+  gdto S_U1 = U1
 
 instance (GDeep prim f) => GDeep prim (M1 i c f) where
-  gdfrom (M1 x) = REPM1 (gdfrom x)
-  gdto (REPM1 x) = M1 (gdto x)
+  gdfrom (M1 x) = S_M1 (gdfrom x)
+  gdto (S_M1 x) = M1 (gdto x)
 
 instance (GDeep prim f , GDeep prim g) => GDeep prim (f :*: g) where
-  gdfrom (x :*: y) = REPPair (gdfrom x) (gdfrom y)
-  gdto (REPPair x y) = (gdto x) :*: (gdto y)
+  gdfrom (x :*: y) = (gdfrom x) :**: (gdfrom y)
+  gdto (x :**: y) = (gdto x) :*: (gdto y)
 
 instance (GDeep prim f , GDeep prim g) => GDeep prim (f :+: g) where
-  gdfrom (L1 x) = REPL1 (gdfrom x)
-  gdfrom (R1 x) = REPR1 (gdfrom x)
+  gdfrom (L1 x) = S_L1 (gdfrom x)
+  gdfrom (R1 x) = S_R1 (gdfrom x)
 
-  gdto (REPL1 x) = L1 (gdto x)
-  gdto (REPR1 x) = R1 (gdto x)
+  gdto (S_L1 x) = L1 (gdto x)
+  gdto (S_R1 x) = R1 (gdto x)
 
 
 ---------------------------
@@ -146,5 +169,5 @@ pyth = Let [Decl "hypSq" (Add (Pow (Var "x") (Lit 2)) (Pow (Var "y") (Lit 2)))]
            (Sqrt (Var "hypSq"))
 
 
-dfromPrim :: (Deep Prims a) => a -> Fix Prims a
+dfromPrim :: (Deep Prims a) => a -> SFix Prims a
 dfromPrim = dfrom
